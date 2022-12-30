dcdb45d3db46d49e1f7d007f08bc0565db1bd459
sinan <aslansinan586@gmail.com>
2022-12-30 20:52:06 +0300

    using handle submit and handlechange in forms

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Formik, Field, Form } from "formik";
+import { Formik } from "formik";
 function App() {
   return (
     <div className="App">
@@ -12,27 +12,28 @@ function App() {
         }}
         onSubmit={async (values) => {
           console.log(values);
-          await new Promise((r) => setTimeout(r, 500));
-          alert(JSON.stringify(values, null, 2));
+          // await new Promise((r) => setTimeout(r, 500));
+          // alert(JSON.stringify(values, null, 2));
         }}
       >
-        <Form>
-          <label htmlFor="firstName">First Name</label>
-          <Field id="firstName" name="firstName" placeholder="Jane" />
-          <br /><br />
-          <label htmlFor="lastName">Last Name</label>
-          <Field id="lastName" name="lastName" placeholder="Doe" />
-          <br /><br />
-          <label htmlFor="email">Email</label>
-          <Field
-            id="email"
-            name="email"
-            placeholder="jane@acme.com"
-            type="email"
-          />
-          <br /><br />
-          <button type="submit">Submit</button>
-        </Form>
+        {({ handleSubmit, handleChange }) => (
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="firstName">First Name</label>
+            <input name="firstName" onChange={handleChange} />
+            <br />
+            <br />
+            <label htmlFor="lastName">Last Name</label>
+            <input name="lastName" onChange={handleChange} />
+            <br />
+            <br />
+            <label htmlFor="email">Email</label>
+            <input name="email" onChange={handleChange} />
+
+            <br />
+            <br />
+            <button type="submit">Submit</button>
+          </form>
+        )}
       </Formik>
     </div>
   );
